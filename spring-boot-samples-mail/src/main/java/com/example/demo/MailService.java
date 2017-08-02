3e1b2ab28ad79b33d4d0a3a717f4ef9965a5571e
huxu <hellohuxu@qq.com>
2017-08-03 01:41:24 +0800

    mail

--- spring-boot-samples-mail/src/main/java/com/example/demo/MailService.java
+++ spring-boot-samples-mail/src/main/java/com/example/demo/MailService.java
@@ -1,10 +1,24 @@
 package com.example.demo;
 
+import org.thymeleaf.context.Context;
+
 /**
  * Created by HUXU on 2017/8/2.
  */
 public interface MailService {
 
+    //发送普通邮件
     void sendSimpleMail(String to, String subject, String content);
 
+    //发送html格式邮件
+    void sendHtmlMail(String to, String subject, String content);
+
+    //发送带附件的邮件
+    void sendAttachmentsMail(String to, String subject, String content, String filePath);
+
+    //发送带静态资源的邮件
+    void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId);
+
+    //发送模板邮件
+    void sendTemplateMail(String to, String subject, Context context, String template);
 }
--- spring-boot-samples-mail/src/test/java/com/example/demo/MailApplicationTests.java
+++ spring-boot-samples-mail/src/test/java/com/example/demo/MailApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.context.Context;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,6 +21,39 @@ public class MailApplicationTests {
 	@Test
 	public void testSimpleMail() throws Exception {
 		MailService.sendSimpleMail("cow-boy@foxmail.com","test simple mail"," hello this is simple mail");
+	}
+
+	@Test
+	public void testHtmlMail() throws Exception {
+		String content="<html>\n" +
+				"<body>\n" +
+				"    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
+				"</body>\n" +
+				"</html>";
+		MailService.sendHtmlMail("cow-boy@foxmail.com","test simple mail",content);
+	}
+
+	@Test
+	public void sendAttachmentsMail() {
+		String filePath="E:\\MainFrame.java";
+		MailService.sendAttachmentsMail("cow-boy@foxmail.com", "主题：带附件的邮件", "有附件，请查收！", filePath);
+	}
+
+	@Test
+	public void sendInlineResourceMail() {
+		String rscId = "11";
+		String content="<html><body>这是有图片的邮件：<img src=\'cid:" + rscId + "\' ></body></html>";
+		String imgPath = "C:\\Users\\huxu\\Desktop\\11.png";
+		MailService.sendInlineResourceMail("cow-boy@foxmail.com", "主题：这是有图片的邮件", content, imgPath, rscId);
+	}
+
+	@Test
+	public void sendTemplateMail() {
+		Context context = new Context();
+		context.setVariable("type", "register");
+		context.setVariable("name", "my friend");
+		String template = "emailTemplate";
+		MailService.sendTemplateMail("cow-boy@foxmail.com","主题：这是模板邮件", context, template);
 	}
 
 }

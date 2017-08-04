56024c3b9433b8e40ab3f02291ccec89bd748d0a
cow-boy <hellohuxu@qq.com>
2017-08-04 10:22:23 +0800

    asyncs

---
+++ spring-boot-samples-asyncs/src/main/java/com/example/demo/AsyncsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncsApplication.class, args);
+	}
+}

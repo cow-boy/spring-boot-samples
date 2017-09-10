2baa33ce4dac861507d8e569c3d233274638f7d2
cow-boy <hellohuxu@qq.com>
2017-09-10 14:41:46 +0800

    rabbitmq

--- spring-boot-samples-rabbitmq/src/test/java/com/example/demo/RabbitmqApplicationTests.java
+++ spring-boot-samples-rabbitmq/src/test/java/com/example/demo/RabbitmqApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CowboyApplicationTests {
+public class RabbitmqApplicationTests {
 
 	@Test
 	public void contextLoads() {
